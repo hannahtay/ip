e7fa32f478ac4fbf7a0cb34d2766a18ee28d017e
hannahtay <heyynuhh@gmail.com>
2025-01-22 20:44:00 +0800

    Added Level 0 - Rename, greet, exit

---
+++ src/main/java/Nyani.java
@@ -0,0 +1,15 @@
+public class Nyani {
+    public static void main(String[] args) {
+        String logo =
+                 "     /ᐠ,,,,∧ \n"
+                + "     (֊⎚-⎚)\n"
+                + "     /⌒   づ\uD83D\uDCD7";
+        System.out.println(logo);
+        System.out.println("Hi there! I'm NYANI ^_^");
+        System.out.println("What shall we do today?");
+        System.out.println("──────────୨ৎ──────────");
+        System.out.println("c U soon! ꈍ ꈍ");
+    }
+
+
+}
