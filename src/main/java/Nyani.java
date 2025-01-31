5de96e7bca448d848d4e610787845d99fc2e0486
hannahtay <heyynuhh@gmail.com>
2025-01-31 11:18:56 +0800

    Added echo function, new class for system messages
    Echoes user inputs
    Made code neater

--- src/main/java/Nyani.java
+++ src/main/java/Nyani.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class Nyani {
     public static void main(String[] args) {
-        String logo =
-                 "     /ᐠ,,,,∧ \n"
-                + "     (֊⎚-⎚)\n"
-                + "     /⌒   づ\uD83D\uDCD7";
-        System.out.println(logo);
-        System.out.println("Hi there! I'm NYANI ^_^");
-        System.out.println("What shall we do today?");
-        System.out.println("──────────୨ৎ──────────");
-        System.out.println("c U soon! ꈍ ꈍ");
+        Scanner sc = new Scanner(System.in);
+        sysMsgs systemOut = new sysMsgs();
+        systemOut.showIntro();
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("list")) {
+                systemOut.listMsg();
+            }
+            else if (input.equalsIgnoreCase("bye")) {
+               systemOut.exitMsg();
+                break;
+            } else {
+                System.out.println("──────────୨ৎ──────────");
+                System.out.println(input);
+                System.out.println("──────────୨ৎ──────────");
+            }
+        }
+        sc.close();
     }
 
 
---
+++ src/main/java/sysMsgs.java
@@ -0,0 +1,24 @@
+public class sysMsgs {
+
+    public void showIntro() {
+        String logo =
+                "     /ᐠ,,,,∧\n"
+                        + "     (֊⎚-⎚)\n"
+                        + "     /⌒   づ\uD83D\uDCD7";
+        System.out.println(logo);
+        System.out.println("Hi there! I'm NYANI ^_^");
+        System.out.println("What shall we do today?");
+    }
+
+    public void exitMsg() {
+        System.out.println("──────────୨ৎ──────────");
+        System.out.println("c U soon! ꈍ ꈍ");
+        System.out.println("──────────୨ৎ──────────");
+    }
+
+    public void listMsg() {
+        System.out.println("──────────୨ৎ──────────");
+        System.out.println("To-do!");
+        System.out.println("──────────୨ৎ──────────");
+    }
+}
